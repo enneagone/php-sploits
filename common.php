4d0dd543c0c58068ba7796fc8ac36b3fd40d1aab
Croquelois <croquelois.adrien@gmail.com>
2020-12-16 22:48:11 +0100

    add style page login

--- common.php
+++ common.php
@@ -19,12 +19,12 @@
 
 <body>
     <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
-        <div class="container"><a class="navbar-brand" href="#">Cyber-Scurity</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand" href="/login.php">Cyber-Security</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav">
-                    <li class="nav-item"><a class="nav-link active" href="/login.php">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/login.php">Login</a></li>
                     <li class="nav-item"><a class="nav-link" href="/sendmessage.php">Send message</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/messages.php">View Messages&nbsp;</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/messages.php">View Messages</a></li>
                 </ul>
                 <form class="form-inline mr-auto" target="_self">
                     <div class="form-group"><label for="search-field"></label><a href="/editprofile.php">Edit profile</a><input class="form-control search-field" type="search" id="search-field" name="search"></div>
--- index.php
+++ index.php
@@ -2,25 +2,40 @@
 require_once 'common.php';
 require_once 'dbfuncs.php';
 
-$getUser = $_REQUEST["username"];
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    if(!empty($_REQUEST['username']) && !empty($_REQUEST['password'])) {
+        $authSQL = "select * from users where username = '" . $_REQUEST['username'] .
+           "' and password = '" . $_REQUEST['password'] . "'";
+        $authed = getSelect($authSQL);
 
-// ' UNION SELECT 1,1,1,1,LOAD_FILE('/etc/passwd'),'1
-
-if(!empty($getUser)) {
-	$query   = "select * from users where username = '" . $getUser . "'";
-	$results = getSelect($query);
-}
-if(!$results) {
-    echo "Unable to find users: " . $_GET["username"];
-}
-else {
-    foreach($results as $row) {
-        echo "User found: <br>";
-        echo "<b>Id:</b> " . $row[0] . "<br>";
-        echo "<b>Username: </b>" . $row[1] . "<br>";
-        echo "<b>Password: </b>" . $row[2] . "<br>";
-        echo "<b>Firstname: </b>" . $row[3] . "<br>";
-        echo "<b>Lastname: </b>" . $row[4] . "<br>";
-        echo "<b>Email: </b>" . $row[5] . "<br>";
+        if(!$authed) {
+            echo 'Invalid login.<br>';
+	    error_log('Loglevel: WARNING | Connection failed: ' . $_REQUEST['username']);
+            die;
+        }
+        else {
+            echo 'Success, you authed! <br>';
+            echo 'SQL Used: ' . $authSQL;
+            $_SESSION['authed'] = true;
+            $_SESSION['userid'] = $authed[0][0];
+            $_SESSION['username'] = $authed[0][1];
+        }
     }
 }
+else {
+?>
+<div class="login-dark">
+    <form method="POST">
+        <h2 class="sr-only">Login Form</h2>
+        <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+        <div class="form-group"><input class="form-control" type="email" name="username" placeholder="Usernale"></div>
+        <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+        <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div>
+    </form>
+</div>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/Profile-Edit-Form.js"></script>
+<?php
+} 
